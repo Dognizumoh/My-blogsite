28ac01e32a18ffaf0bab5ba89201089cb835cf3d
Dognizumoh <idafolo16@gmail.com>
2022-03-30 13:54:40 +0000

    added route

---
+++ src/pages/About.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import withLayout from './withLayout'
+
+function About() {
+  return (
+    <div>
+    <h1>Welcome to my About me</h1>
+    <div>
+    <p>Jessica Marie Alba was born in Pomona,
+     California[10] on April 28, 1981, the daughter of Catherine Louisa (née Jensen)
+      and Mark David Alba. Her mother has Danish, Welsh, German, English and French ancestry, 
+      while her paternal grandparents, who were born in California,
+       were both the children of Mexican immigrants. She has a younger brother
+    , Joshua. Her third cousin, once removed,
+     is writer Gustavo Arellano.[1</p>
+    <p>
+    Jessica Marie Alba was born in Pomona, California[10] on April 28, 1981,
+     the daughter of Catherine Louisa (née Jensen) and Mark David Alba.
+      Her mother has Danish, Welsh, German, English and French ancestry, 
+      while her paternal grandparents, who were born in California, 
+      were both the children of Mexican immigrants.[11] She has a younger brother
+    , Joshua. Her third cousin, once removed, 
+    is writer Gustavo Arellano.
+    
+    </p>
+    <p>Jessica Marie Alba was born in Pomona,
+     California[10] on April 28, 1981, the daughter of Catherine Louisa (née Jensen)
+      and Mark David Alba. Her mother has Danish, Welsh, German, English and French ancestry,
+       while her paternal grandparents, who were born in California,
+        were both the children of Mexican immigrants.[11] She has a younger brother,
+     Joshua. Her third cousin, once removed, is writer Gustavo Arellano.
+    </p>
+    
+    </div>
+    </div>
+  )
+}
+
+export default withLayout(About)
---
+++ src/pages/Blog.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function Blog() {
+  return (
+    <div>
+    <h1>Welcome to my Blogpage</h1>
+    <div>
+    <p>Jessica Marie Alba was born in Pomona,
+     California[10] on April 28, 1981, the daughter of Catherine Louisa (née Jensen)
+      and Mark David Alba. Her mother has Danish, Welsh, German, English and French ancestry, 
+      while her paternal grandparents, who were born in California,
+       were both the children of Mexican immigrants. She has a younger brother
+    , Joshua. Her third cousin, once removed,
+     is writer Gustavo Arellano.[1</p>
+    <p>
+    Jessica Marie Alba was born in Pomona, California[10] on April 28, 1981,
+     the daughter of Catherine Louisa (née Jensen) and Mark David Alba.
+      Her mother has Danish, Welsh, German, English and French ancestry, 
+      while her paternal grandparents, who were born in California, 
+      were both the children of Mexican immigrants.[11] She has a younger brother
+    , Joshua. Her third cousin, once removed, 
+    is writer Gustavo Arellano.
+    
+    </p>
+    <p>Jessica Marie Alba was born in Pomona,
+     California[10] on April 28, 1981, the daughter of Catherine Louisa (née Jensen)
+      and Mark David Alba. Her mother has Danish, Welsh, German, English and French ancestry,
+       while her paternal grandparents, who were born in California,
+        were both the children of Mexican immigrants.[11] She has a younger brother,
+     Joshua. Her third cousin, once removed, is writer Gustavo Arellano.
+    </p>
+    </div>
+    </div>
+  )
+}
+
+export default(Blog)
